3f770230b723e3c168596d918a5082b59ad58638
Sivhila <anninachilombe@gmail.com>
2023-02-21 06:47:08 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,8 +17,7 @@ int main(void)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if (!((ones == tens) || (tens == hundreds) ||
-							(tens > ones) || (hundreds > tens))) /*eliminates repitition*/
+				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens))) /*eliminates repitition*/
 				{
 					putchar(hundreds);
 					putchar(tens);
@@ -33,6 +32,7 @@ int main(void)
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
 
