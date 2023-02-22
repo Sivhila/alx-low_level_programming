a5ef0c534b892dc9552687a23a3965a826e8f092
Sivhila <anninachilombe@gmail.com>
2023-02-22 13:01:27 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 /**
  * _abs - Computes the absolute value of an integer.
  * @c: The number to be computed.
- * Retrurn: Absolute value of number or zero
+ * Return: Absolute value of number or zero
  */
 
 int _abs(int c)
@@ -13,7 +13,7 @@ int _abs(int c)
 	{
 	int abs_val;
 
-	abs_val = c * -1;
+	abs_val = c *-1;
 	return (abs_val);
 	}
 	return (c);
