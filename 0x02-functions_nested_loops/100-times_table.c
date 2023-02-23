42b1bb63f4b66f3ba79b9b1575d40bb5f7f31b0b
Sivhila <anninachilombe@gmail.com>
2023-02-23 04:40:00 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,7 +18,7 @@ void print_times_table(int n)
 	_putchar('0');
 	for (mult = 1; mult <= n; mult++)
 	{
-	_putchar('0');
+	_putchar(',');
 	_putchar(' ');
 	prod = num * mult;
 	if (prod <= 99)
@@ -26,7 +26,7 @@ void print_times_table(int n)
 	if (prod >= 100)
 	{
 	_putchar((prod / 100) + '0');
-	_putchar((prod / 10) % 10 + '0');
+	_putchar(((prod / 10)) % 10 + '0');
 	}
 	else if (prod <= 99 && prod >= 10)
 	{
