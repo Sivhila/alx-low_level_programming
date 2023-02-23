d9bf1e819bbd72744ad14d13d25278fe8c8b9840
Sivhila <anninachilombe@gmail.com>
2023-02-23 09:04:18 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,7 +14,7 @@ int main(void)
 	while (i < 50)
 	{
 	if (i == 0)
-	printf(", %ld", j);
+	printf("%ld", j);
 	else if (i == 1)
 	printf(", %ld", k);
 	else
@@ -27,5 +27,4 @@ int main(void)
 	}
 	printf("\n");
 	return (0);
-
 }
