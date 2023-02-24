e86dde7f59dc385c849df43331683a6727315c26
Sivhila <anninachilombe@gmail.com>
2023-02-23 19:14:07 -0800

    0-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * main - Contains all prototype for the functions
+ * used in 0x03-debugging project.
+ */
+
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
