9cb93b55eb61ee0b324518725d728bc4fd9a69da
Sivhila <anninachilombe@gmail.com>
2023-06-15 14:28:12 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * The main function call starts from int main.
+ * *s - represents a pointer to a character.
+ * str - is n array of characters.
+ * _puts_recursion - a function that recursively prints out a string.
+ */
+
+void _puts_recursion(char *s)
+{
+
+	if (*s <= '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
